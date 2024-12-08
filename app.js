dc5f0b632fedd7819055ab694c1dc5adcd92bb16
Gonzalo Sanchez <gonza.piru@gmail.com>
2024-12-07 23:18:02 -0300

    segunda pre entrega sanchez gonzalo

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const exphbs = require('express-handlebars');
+const { engine } = require('express-handlebars');  // Corrección realizada
 const path = require('path');
 
 const productRoutes = require('./routes/products');
 const cartRoutes = require('./routes/carts');
 const viewRoutes = require('./routes/views.routes');
-const { initializeSocket } = require('../services/SocketManager');
+const { initializeSocket } = require('./services/SocketManager');
 
 const app = express();
 const PORT = 8080;
 
 // Configuración de Handlebars
-app.engine('handlebars', exphbs());
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));  // Asegurarse de que 'views' esté correctamente configurado
 
 // Middleware global
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));  // Servir archivos estáticos
 
 // Middleware de registro de solicitudes
 app.use((req, res, next) => {
@@ -35,8 +35,11 @@ app.use('/', viewRoutes);
 
 // Ruta raíz
 app.get('/', (req, res) => {
-    res.redirect('/home');
+    res.redirect('home');  // Redirige a la página de inicio
 });
+
+// Manejo de favicon (para evitar errores de favicon.ico)
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 // Manejo de rutas no encontradas
 app.use((req, res) => {
--- services/socketManager.js
+++ services/socketManager.js
@@ -1,17 +1,15 @@
-const { Server } = require('socket.io');
+// services/SocketManager.js
+const socketio = require('socket.io');
 
-function initializeSocket(server) {
-    const io = new Server(server);
-
+const initializeSocket = (server) => {
+    const io = socketio(server);
     io.on('connection', (socket) => {
         console.log('Nuevo cliente conectado');
         socket.on('disconnect', () => {
             console.log('Cliente desconectado');
         });
     });
-
-    return io;
-}
+};
 
 module.exports = { initializeSocket };
 
