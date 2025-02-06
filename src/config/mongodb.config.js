7d9dbb11708166af20468718e6d3c6a0a151a72b
Gonzalo Sanchez <gonza.piru@gmail.com>
2025-02-06 09:02:38 -0300

    arreglos en las rutas

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
 
 export const connectMongoDB = () => {
-    mongoose.connect('mongodb://localhost:27017/Project01', { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
-    }).then(() => {
-        console.log('Conectado a MongoDB');
-    }).catch((error) => {
-        console.log('Error al conectar a MongoDB', error);
-    });
+    mongoose.connect('mongodb://localhost:27017/Project01')
+        .then(() => {
+            console.log('Conectado a MongoDB');
+        })
+        .catch((error) => {
+            console.error('Error al conectar a MongoDB', error);
+        });
 };
